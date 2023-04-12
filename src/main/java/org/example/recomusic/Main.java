e385a58e350d2df870f9d0c15ae3e24269194522
Rievvy <thiagoalmeidadepaiva@gmail.com>
2023-04-12 19:01:40 -0300

    add to sample

--- src/main/java/org/example/recomusic/Main.java
+++ src/main/java/org/example/recomusic/Main.java
@@ -1,13 +1,20 @@
 package org.example.recomusic;
 
 import lombok.Data;
+
+import java.io.IOException;
+
 @Data
 public class Main {
 
     public static void main(String[] args) {
         Graph graph = new Graph();
-        graph = graph.loadGraph("graph.recomusic");
-        System.out.println(graph.getVertices().size());
-        Screen s = new Screen(graph);
+        try {graph = graph.loadGraph("graph.recomusic");
+            System.out.println(graph.getVertices().size());
+            Screen s = new Screen(graph);} catch (IOException e) {
+            graph = Graph.createGraph("dataset.csv", 50000, 10);
+            graph.saveGraph("graph.recomusic");
+        }
+
     }
 }
