2bf809115039c067438a1bdb3f40b740485baef2
william <williambatista958@gmail.com>
2023-04-12 22:30:17 -0300

    creating interface button functions

--- src/main/java/org/example/recomusic/Main.java
+++ src/main/java/org/example/recomusic/Main.java
@@ -9,12 +9,13 @@ public class Main {
 
     public static void main(String[] args) {
         Graph graph = new Graph();
-        try {graph = graph.loadGraph("graph.recomusic");
+        try {
+            graph = graph.loadGraph("graph.recomusic");
             System.out.println(graph.getVertices().size());
-            Screen s = new Screen(graph);} catch (IOException e) {
+            Screen s = new Screen(graph);}
+        catch (IOException e) {
             graph = Graph.createGraph("dataset.csv", 50000, 10);
             graph.saveGraph("graph.recomusic");
         }
-
     }
 }
--- src/main/java/org/example/recomusic/Screen.java
+++ src/main/java/org/example/recomusic/Screen.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class Screen extends JFrame {
@@ -13,7 +14,7 @@ public class Screen extends JFrame {
     private  ArrayList<Vertex> sample;
     private Vertex music;
     private final JTextField field;
-    JList lst;
+
 
     private Graph graph;
 
@@ -23,9 +24,20 @@ public class Screen extends JFrame {
         sample = new ArrayList<Vertex>();
         music = new Vertex();
         field = new JTextField();
-        JFrame frame1 = new JFrame();
+        DefaultListModel listSample = new DefaultListModel();
+        DefaultListModel listRecommendation = new DefaultListModel();
+        JList lst = new JList(listSample);
+        JList lst2 = new JList(listRecommendation);
+        JScrollPane jpListSample = new JScrollPane(lst);
+        JScrollPane jpListRecommendation = new JScrollPane(lst2);
 
-        frame1.setSize(500, 500);
+        jpListSample.setBounds(100, 600, 400, 400);
+        jpListRecommendation.setBounds(600, 600, 400, 400);
+        JFrame frame1 = new JFrame();
+        frame1.add(jpListSample);
+        frame1.add(jpListRecommendation);
+
+        frame1.setSize(1500, 1000);
 
         frame1.setLayout(null);
 
@@ -34,13 +46,13 @@ public class Screen extends JFrame {
         JButton button1 = new JButton("Escolher Música");
         button1.setBounds(100, 100, 300, 40);
 
-        JButton button2 = new JButton("Ver Sample");
+        JButton button2 = new JButton("Gerar Recomendações");
         button2.setBounds(100, 150, 300, 40);
 
         JButton button3 = new JButton("Limpar Sample");
         button3.setBounds(100, 200, 300, 40);
 
-        JButton button4 = new JButton("Mostrar Recomendações");
+        JButton button4 = new JButton("Limpar Recomendações");
         button4.setBounds(100, 250, 300, 40);
 
         JButton button5 = new JButton("Fechar");
@@ -51,11 +63,8 @@ public class Screen extends JFrame {
                 JFrame frame = new JFrame();
                 JButton search = new JButton("Procurar");
                 JButton button = new JButton("Adicionar");
-
                 frame.setSize(500, 500);
-
                 frame.setLayout(null);
-
                 frame.setVisible(true);
 
                 search.addActionListener(new ActionListener() {
@@ -64,23 +73,15 @@ public class Screen extends JFrame {
                         String trackID = field.getText();
 
                         music = graph.searchMusic(trackID);
-
-
-
                         if(music != null) {
                             JOptionPane.showMessageDialog(null,
                                   "Nome do Artista: " + music.getArtists() +
                                   "\nNome da Música: " + music.getTrackName());
-
-
                         }
                         else{
                             JOptionPane.showMessageDialog(null, "Impossível encontrar música, tente " +
                                     "com outro ID");
                         }
-
-
-
                     }
                 });
 
@@ -88,56 +89,64 @@ public class Screen extends JFrame {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         String trackID = field.getText();
-
                         music = graph.searchMusic(trackID);
 
                         if(music != null) {
                            sample.add(music);
                            JOptionPane.showMessageDialog(null, "Música adicionada à playlist");
                         }
-                        else{
-                            JOptionPane.showMessageDialog(null, "Impossível encontrar música, tente " +
-                                    "com outro ID");
-                        }
+                        else{JOptionPane.showMessageDialog(null, "Impossível encontrar música, tente " + "com outro ID");}
+
+                        listSample.add(listSample.size()," Nome " +sample.get(listSample.size()).getTrackName() + " Artirta: " + sample.get(listSample.size()).getArtists());
+                        frame1.add(jpListSample);
+                        frame.setVisible(false);
                     }
                 });
-
                 search.setBounds(310, 100, 100, 20);
                 button.setBounds(150, 400, 200, 40);
                 field.setBounds(100, 100, 200, 20);
-
                 frame.add(search);
                 frame.add(field);
                 frame.add(button);
-
             }
         });
-
         button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame frame = new JFrame();
+                if(listSample.size() > 0 ){
+                    JOptionPane.showMessageDialog(null, "Carregando recomendação, aguarde!");
+                    try{
+                        List<Vertex> listR = graph.recommend(sample, 3);
+                        listRecommendation.add(listRecommendation.size(), listR.get(0).getTrackName() + " - " + listR.get(0).getArtists());
+                        listRecommendation.add(listRecommendation.size(), listR.get(1).getTrackName() + " - " + listR.get(1).getArtists());
+                        listRecommendation.add(listRecommendation.size(), listR.get(2).getTrackName() + " - " + listR.get(2).getArtists());
+                    }catch (Exception erro){
+                        JOptionPane.showMessageDialog(null, "Não Foi possivel gerar uma recomendação! \nERRO:" + erro.getMessage());
+                    }
+                }else{ JOptionPane.showMessageDialog(null, "Primeiro crie uma lista de musicas!");}
 
-                DefaultListModel<String> lista = new DefaultListModel<>();
-                for (Vertex v : sample) {
-                    lista.addElement(v.getTrackName());
-                }
-
-                lst = new JList<>(lista);
-
-                JScrollPane jp = new JScrollPane();
-
-                jp.setViewportView(lst);
-                jp.setBounds(100, 100, 200, 200);
-
-                frame.add(lst);
-                System.out.println(sample.get(0).getTrackName());
-                frame.setVisible(true);
-                frame.setLayout(null);
-                frame.setSize(500, 500);
             }
         });
 
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               listSample.removeAllElements();
+            }
+        });
+
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                listRecommendation.removeAllElements();
+            }
+        });
+        button5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
 
         frame1.add(button1);
         frame1.add(button2);
@@ -145,12 +154,4 @@ public class Screen extends JFrame {
         frame1.add(button4);
         frame1.add(button5);
     }
-
-    public JList getList () {
-        return lst;
-    }
-
-
-
-
 }
